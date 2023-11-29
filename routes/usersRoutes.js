e70a2a367da76c77deea2234c6eea24f0e72031c
kirill23111 <radyushinkirill@yandex.ru>
2023-11-29 11:54:40 +0200

    ПР14

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -28,17 +28,17 @@ const express = require('express');
 const usersController = require('../controllers/users');
 const authMiddleware = require('../middlewares/auth');
 const {
-  getUserByIdValidation,
   updateProfileValidation,
   updateAvatarValidation,
+  getUserByIdValidation,
 } = require('../middlewares/validation');
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getUsers);
-router.patch('/me', authMiddleware, updateProfileValidation.updateProfile);
-router.patch('/me/avatar', authMiddleware, updateAvatarValidation.updateAvatar);
-router.get('/me', authMiddleware, getCurrentUser);
-router.get('/:userId', authMiddleware, getUserByIdValidation.getUserById);
+router.get('/', authMiddleware, usersController.getUsers);
+router.patch('/me', authMiddleware, updateProfileValidation, usersController.updateProfile);
+router.patch('/me/avatar', authMiddleware, updateAvatarValidation, usersController.updateAvatar);
+router.get('/me', authMiddleware, usersController.getCurrentUser);
+router.get('/:userId', authMiddleware, getUserByIdValidation, usersController.getUserById);
 
 module.exports = router;
